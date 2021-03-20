596b13509268b8a00300a8f3d8cc4d6ffd9a1ff8
Relise228 <andreysmit43@gmail.com>
2021-03-20 18:47:38 +0200

    deploy version

--- server.js
+++ server.js
@@ -1,17 +1,19 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
+const path = require('path');
+
 const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
-// app.use(express.static(path.join(__dirname, 'build')));
+app.use(express.static(path.join(__dirname, 'build')));
 
-// app.get('/', (req, res) => {
-//   res.sendFile(path.join(__dirname, 'build', 'index.html'));
-// });
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
 
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
